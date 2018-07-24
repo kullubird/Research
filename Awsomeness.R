f1406bfa990cce416e9f1fa1128dffc167fca4b4
kullubird <kevindsouza@live.com>
2018-07-25 00:46:10 +0530

    further champ analysis

--- Awsomeness.R
+++ Awsomeness.R
@@ -19,7 +19,7 @@ ban3=unlist(global$ban_3)
 ban4=unlist(global$ban_4)
 ban5=unlist(global$ban_5)
 
-
+all
 
 allBan=paste(ban1,ban2,ban3,ban4,ban5,collapse =" ")
 
@@ -43,7 +43,8 @@ plot(wordfreq$Var1,wordfreq$Freq)
 ggplot(wordfreq)
  
 barplot(table(unlist(words)))
-barplot(res$Freq,names.arg = res$Var1,las=2)
+barplot(res$Freq,names.arg = res$Var1,las=2,ylim=c(0,2500)   )
+
 help(barplot)
 global.Corpus=Corpus(VectorSource(allBan))
 
@@ -57,4 +58,23 @@ global.Clean<-tm_map(global.Clean,stripWhitespace)
 global.Clean<-tm_map(global.Clean,stemDocument)                     
                                 
 wordcloud(global.Clean,max.words = 300,random.color = TRUE,random.order=FALSE)
+
+
+
+#new df with bans and picks
+
+df1<-read.csv('D:/Studies/Research/Dataset - Kaggle/bans2.csv',sep=',')
+df2<-read.csv('D:/Studies/Research/Dataset - Kaggle/matchinfo.csv',sep=',')
+View(df1)
+df1=subset(df1,select=-c(Team,ban_2))
+df1
+df3<-merge(df1[0:5],df2,by.x="Address",by.y="Address")
+
+
+uniqueChamps=read.csv('D:/Studies/Research/myMine/Chapions.csv')
+uniqueChamps=as.data.frame(uniqueChamps)
+View(uniqueChamps)
+uniqueChamps=subset(uniqueChamps, select = c(uniqueChamps$data__.__name,uniqueChamps$))
+
+View(df3)
                      
